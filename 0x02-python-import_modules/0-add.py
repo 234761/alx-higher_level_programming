60c3095028b00e9116e6dfdfef50f66c6a617582
234761 <dag234761@gmail.com>
2022-07-03 08:06:32 -0700

    my first import commit

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+#!/bin/bash/pthon3
+# 0_add.py
+# dagnachew 234761 <dag234761@gmail.com>
+
+if __name__ == "__main__":
+    """Print the sum of 1 and 2."""
+    from add_0 import ad
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
+
